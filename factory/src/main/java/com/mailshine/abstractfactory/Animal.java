1f1fe15ffa94dcbd66238f9cd64349364fa6bbb1
shinemon <58699255+shinemon@users.noreply.github.com>
2020-02-26 23:31:36 +0000

    updating factory pattern module

---
+++ factory/src/main/java/com/mailshine/abstractfactory/Animal.java
@@ -0,0 +1,6 @@
+package com.mailshine.abstractfactory;
+
+public interface Animal {
+
+    public String getSound();
+}
---
+++ factory/src/main/java/com/mailshine/abstractfactory/Cat.java
@@ -0,0 +1,8 @@
+package com.mailshine.abstractfactory;
+
+public class Cat implements Animal {
+    @Override
+    public String getSound() {
+        return null;
+    }
+}
