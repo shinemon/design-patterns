c81c7cb6041897c9314127a573f057fb4cc965a8
shinemon <58699255+shinemon@users.noreply.github.com>
2020-03-07 17:36:41 +0000

    updating Builder Pattern example module

--- builder/src/main/java/com/mailshine/builder/with/boilerplatecode/Employee.java
+++ builder/src/main/java/com/mailshine/builder/with/boilerplatecode/Employee.java
@@ -31,10 +31,43 @@ public class Employee {
         return locationAddress;
     }
 
-    private Employee(EmployeeBuilder builder){
-
+    private Employee(EmployeeBuilder builder) {
+        this.id = builder.id;
+        this.name = builder.name;
+        this.age = builder.age;
+        this.department = builder.department;
+        this.locationAddress = builder.locationAddress;
     }
 
     public static class EmployeeBuilder {
+        private String id;
+        private String name;
+        private int age;
+        private String department;
+        private String locationAddress;
+
+        public EmployeeBuilder(String id, String name) {
+            this.id = id;
+            this.name = name;
+        }
+
+        public EmployeeBuilder setAge(int age) {
+            this.age = age;
+            return this;
+        }
+
+        public EmployeeBuilder setDepartment(String department) {
+            this.department = department;
+            return this;
+        }
+
+        public EmployeeBuilder setLocationAddress(String locationAddress) {
+            this.locationAddress = locationAddress;
+            return this;
+        }
+
+        public Employee build() {
+            return new Employee(this);
+        }
     }
 }
